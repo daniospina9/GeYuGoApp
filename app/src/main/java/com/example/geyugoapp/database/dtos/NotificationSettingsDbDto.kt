f06f395952d83198f0c7185c1182c5ab19d0c346
daniospina <ingedanielospina9@gmail.com>
2025-08-20 16:16:52 -0500

    fix/added foreignKey to NotificationSettingsDbDto

--- app/src/main/java/com/example/geyugoapp/database/dtos/NotificationSettingsDbDto.kt
+++ app/src/main/java/com/example/geyugoapp/database/dtos/NotificationSettingsDbDto.kt
@@ -1,9 +1,20 @@
 package com.example.geyugoapp.database.dtos
 
 import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "notification_settings")
+@Entity(
+    tableName = "notification_settings",
+    foreignKeys = [
+        ForeignKey(
+            entity = UserDbDto::class,
+            parentColumns = ["id"],
+            childColumns = ["userId"],
+            onDelete = ForeignKey.CASCADE
+        )
+    ]
+)
 data class NotificationSettingsDbDto(
     @PrimaryKey(autoGenerate = true)
     val id: Long = 0L,
